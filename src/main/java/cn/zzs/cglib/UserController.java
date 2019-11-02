3d0e67a7d295dd542b4e6debb6da358c970317c3
ZhangZiSheng001 <18826241741@163.com>
2019-11-02 23:25:10 +0800

    处理冲突

--- src/main/java/cn/zzs/cglib/UserController.java
+++ src/main/java/cn/zzs/cglib/UserController.java
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 package cn.zzs.cglib;
 
 /**
@@ -25,28 +24,3 @@ public class UserController {
 		System.out.println( "查找用户" );
 	}
 }
-=======
-package cn.zzs.cglib;
-/**
- * @ClassName: UserController
- * @Description: 对用户进行操作的Controller
- * @author: zzs
- * @date: 2019年8月31日 下午4:24:13
- */
-public class UserController {
-	public void save() {
-		System.out.println("增加用户");
-	}
-	public void delete() {
-		System.out.println("删除用户");
-	}
-	public void update() {
-		System.out.println("修改用户");
-	}
-	public void find() {
-		System.out.println("查找用户");
-	}
-}
-
-	
->>>>>>> refs/remotes/origin/master
